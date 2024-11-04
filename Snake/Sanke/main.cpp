d86dee3a649b570d3fdf1376c3c4870f9dd15ca9
KhiemVo-Uit <24730039@ms.uit.edu.vn>
2024-11-04 13:21:19 +0700

    Update main.cpp

--- Snake/Sanke/main.cpp
+++ Snake/Sanke/main.cpp
@@ -35,4 +35,21 @@ void Logic()
         prevX = prev2X;
         prevY = prev2Y;
     }
+      switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
 }
