aeb81182bbf8a395e1db13725548f2c8f6e46c8e
Loily-Difu <24730042@ms.uit.edu.vn>
2024-11-04 14:42:12 +0700

    Update main.cpp

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 using namespace std;
 
+// Khai báo biến
 bool gameOver;
 const int width = 40;
 const int height = 15;
@@ -136,6 +137,36 @@ void Draw()
 
     SetColor(11); // Màu xanh dương nhạt cho chữ
     cout << "Diem: " << score << endl;
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            if (dir != RIGHT) dir = LEFT; // Không cho di chuyển ngược lại
+            break;
+        case 'd':
+            if (dir != LEFT) dir = RIGHT; // Không cho di chuyển ngược lại
+            break;
+        case 'w':
+            if (dir != DOWN) dir = UP; // Không cho di chuyển ngược lại
+            break;
+        case 's':
+            if (dir != UP) dir = DOWN; // Không cho di chuyển ngược lại
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        case 'e': // Phím để bật/tắt tăng tốc
+            speedBoostActive = !speedBoostActive; // Đảo ngược trạng thái tăng tốc
+            speed = speedBoostActive ? 80 : 150; // Giảm tốc độ khi bật tăng tốc
+            break;
+        }
+    }
+}
+
 
 void Logic()
 {
