93f6914ffa6b02177aaf5239804f461a79ef1f30
cuongtd1609 <24730018@ms.uit.edu.vn>
2024-11-04 10:30:16 +0700

    Update main.cpp

--- Snake/Sanke/main.cpp
+++ Snake/Sanke/main.cpp
@@ -19,3 +19,35 @@ int fruitsEaten = 0;
 int specialFruitScore = 30; // Điểm cho thức ăn đặc biệt
 bool speedBoostActive = false; // Biến kiểm soát trạng thái tăng tốc
 int speed = 150; // Tốc độ di chuyển mặc định
+// Hàm đổi màu chữ
+void SetColor(int value)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), value);
+}
+
+// Hàm di chuyển con trỏ tới vị trí (x, y) trên console
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Hàm ẩn con trỏ chuột để không bị chớp nháy khi di chuyển rắn
+void HideCursor()
+{
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.bVisible = false;
+    cursorInfo.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+// Hàm hiện con trỏ chuột khi cần
+void ShowCursor()
+{
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.bVisible = true;
+    cursorInfo.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
