ad6ffa68e04b18816e32fb102353f40c8b0e60ba
Loily-Difu <24730042@ms.uit.edu.vn>
2024-11-04 15:39:36 +0700

    Update main.cpp

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -138,6 +138,35 @@ void Draw()
     SetColor(11); // Màu xanh dương nhạt cho chữ
     cout << "Diem: " << score << endl;
 
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            if (dir != RIGHT) dir = LEFT; // Không cho di chuyển ngược lại
+            break;
+        case 'd':
+            if (dir != LEFT) dir = RIGHT; // Không cho di chuyển ngược lại
+            break;
+        case 'w':
+            if (dir != DOWN) dir = UP; // Không cho di chuyển ngược lại
+            break;
+        case 's':
+            if (dir != UP) dir = DOWN; // Không cho di chuyển ngược lại
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        case 'e': // Phím để bật/tắt tăng tốc
+            speedBoostActive = !speedBoostActive; // Đảo ngược trạng thái tăng tốc
+            speed = speedBoostActive ? 80 : 150; // Giảm tốc độ khi bật tăng tốc
+            break;
+        }
+    }
+}
+
 void Logic()
 {
     int prevX = tailX[0];
